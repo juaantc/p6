5539a0747e268ec8b475216607a04e3102ffb39a
Juan Trapero <i42trcaj@uco.es>
2025-12-11 19:51:47 +0100

    new

--- src/counter/counter.h
+++ src/counter/counter.h
@@ -1,10 +1,7 @@
 #ifndef COUNTER_H
 #define COUNTER_H
 
-#include "../persons/persons.h"
-#include "../stage/stage.h"
-#include "../catalog/catalog.h"
-#include "../gclist/gclist.h"
+#include "iostream"
 
 class Counter{
 
@@ -13,35 +10,31 @@ class Counter{
             int min_;
             int max_;
 
+            void checkBounds();
+
     public:
-            Counter();
-            int Get(int value);
-            int GetMin(int min);
-            int GetMax(int max);
+            Counter(int value = 0, int min = 0, int max = 1000);
 
-}
+            int Get() const {return value_;}
+            int GetMin() const {return min_;}
+            int GetMax() const {return max_;}
 
+            Counter& operator=(int v);
+            Counter& operator=(const Counter &c);
 
+            Counter& operator++();     
+            Counter operator++(int);   
+            Counter& operator--();    
+            Counter operator--(int); 
+            
+            friend Counter operator+(const Counter& c, int v);
+            friend Counter operator+(int v, const Counter& c);
+            friend Counter operator-(const Counter& c, int v);
+            friend Counter operator-(int v, const Counter& c);
 
+            friend std::ostream& operator<<(std::ostream& os, const Counter& c);
+            friend std::istream& operator>>(std::ostream& is, Counter& c);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 #endif
